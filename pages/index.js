213252f9945d488c26f5c251d905e33aa4d94159
Sora <jethrosimbulan.dev@gmail.com>
2022-10-06 15:08:11 +0800

    edited the content of portfolio

--- pages/index.js
+++ pages/index.js
@@ -150,10 +150,13 @@ export default function Home () {
           <div>
             <h3 className='text-3xl py-1 dark:text-white '>Portofolio</h3>
             <p className='text-md py-2 leading-8 text-gray-800 dark:text-gray-200'>
-              Since the beginning of my journey as a freelance designer and
+              Since the beginning of my journey as a freelance front-end
               developer, Ive done remote work for
-              <span className='text-orange-500'> agencies </span>
-              consulted for <span className='text-orange-500'>startups </span>
+              <span className='text-orange-500'>
+                {' '}
+                small to medium businesses{' '}
+              </span>
+              , consulted for <span className='text-orange-500'>startups </span>
               and collaborated with talanted people to create digital products
               for both business and consumer use.
             </p>
