e07c6673f218790d77a4503a99da564a8c43b84d
Sora <jethrosimbulan.dev@gmail.com>
2022-10-06 13:20:39 +0800

    update color of the website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: "class",
+  darkMode: 'class',
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
       fontFamily: {
-        burtons: "burtons",
-        poppins: ["Poppins", "sans-serif"],
-      },
-    },
+        sinisuka: 'sinisuka',
+        poppins: ['Poppins', 'sans-serif']
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
