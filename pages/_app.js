82bbcee2405a73627e6fa64d9cbc5af9959dc771
Jethro S <34934587+sora-dev@users.noreply.github.com>
2022-11-28 17:06:05 +0800

    remove analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,11 @@ import '../styles/globals.css'
 import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Analytics />
-    </>
-  );
+  return 
+  <>
+  <Component {...pageProps} />
+  <Analytics />
+  </>
 }
 
 export default MyApp
